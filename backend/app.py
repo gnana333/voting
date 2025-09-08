1652677b3da66422e13f99bf833618a5013afe4a
Gowni Gnanendra Reddy <140694819+gnana333@users.noreply.github.com>
2025-09-08 08:40:39 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -654,4 +654,6 @@ if __name__ == '__main__':
     except Exception as e:
         print(f"Database initialization error: {e}")
     
-    app.run(debug=True, host='0.0.0.0', port=5000) 
+
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
